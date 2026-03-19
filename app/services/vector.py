e31b8b139ded67ebb63bd00e562d14be2b6d7956
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-03-20 01:47:12 +0530

    migrate from chromadb to qdrant

--- app/services/vector.py
+++ app/services/vector.py
@@ -1,21 +1,89 @@
 import chromadb
+import math
 
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, PointStruct, VectorParams
+import uuid 
 from app.core.config import settings
 from app.core.logger import get_logger
+from app.utils.compression import compress_code ,decompress_code
 
 logger = get_logger(__name__)
 
 chroma_client = chromadb.PersistentClient(path=settings.CHROMA_PATH)
 
 
+client = QdrantClient(
+    url=settings.QDRANT_URL, 
+    api_key=settings.QDRANT_API_KEY,
+)
+
+print(client.get_collections())
+
 def get_collection(repo_id: str):
     return chroma_client.get_or_create_collection(
         name = f"repo_{repo_id}",
         metadata={"hnsw:space": "cosine"}
     )
+def get_or_create_collection(repo_id:str,vector_size: int):
+    collection_name = f"repo_{repo_id}"
+    try:
+        client.get_collection(collection_name=collection_name)
+    except:
+        client.create_collection(
+            collection_name=collection_name,
+            vectors_config= VectorParams(
+                size=vector_size,
+                distance = Distance.COSINE
+                )
+            )
+    return collection_name
 
-def store_embeddings(repo_id: str, chunks: list[dict])->int:
-    collection = get_collection(repo_id)
+
+def store_embeddings_batch(repo_id: str, chunks: list[dict]) -> int:
+    collection = get_or_create_collection(repo_id, len(chunks[0]["vector"]))
+
+    batch_size = 10
+    batch_length = math.ceil(len(chunks)/batch_size)
+    batch_count = 1 
+
+    for i in range(0, len(chunks), batch_size) :
+        batch = chunks[i:i+batch_size+1]
+        logger.info(f"Storing batch {batch_count}/{batch_length} ")
+        points = [
+            PointStruct(
+                id      = str(uuid.uuid4()),
+                vector  = chunk["vector"],
+                payload = {
+                    "name":       chunk["name"],
+                    "file":       chunk["file"],
+                    "language":   chunk["language"],
+                    "type":       chunk["type"],
+                    "start_line": chunk["start_line"],
+                    "end_line":   chunk["end_line"],
+                    "code":       compress_code(chunk["code"])
+                }
+            )
+            for chunk in batch
+        ]
+        client.upsert(
+            collection_name=collection,
+            points= points
+        )
+        batch_count += 1 
+    return len(chunks)
+
+
+def get_chunks_count(repo_id:str) ->int:
+    collection = f"repo_{repo_id}"
+    try:
+        info = client.get_collection(collection_name=collection)
+        return info.points_count
+    except:
+        return 0 
+        
+
+    collection = get_collection
 
     existing_count = collection.count()
     if existing_count > 0:
@@ -49,31 +117,44 @@ def store_embeddings(repo_id: str, chunks: list[dict])->int:
     return len(chunks)
 
 def search_embeddings(repo_id:str, query_vector:str, top_k : int = 5) ->list[dict] :
-    collection  = get_collection(repo_id=repo_id)
+    logger.debug("QUery Reached here")
+    collection  = f"repo_{repo_id}"
 
-    if collection.count() == 0:
-        raise Exception(f"No embeddings found for repo {repo_id}")
+    try:
+        results = client.query_points(
+            collection_name = collection,
+            query = query_vector,
+            limit = top_k
+        )
+        results = results.points
+        logger.debug(type(results))
+    except Exception as e :
+        logger.debug(str(e))
+    
 
 
-    results = collection.query(query_embeddings=query_vector,
-                               n_results=top_k, 
-                               include=["documents","metadatas","distances"])
 
     chunks = []
 
-    for i in range(len(results["ids"][0])):
+    for r in results:
         chunks.append({
-            "code":       results["documents"][0][i],
-            "file":       results["metadatas"][0][i]["file"],
-            "language":   results["metadatas"][0][i]["language"],
-            "type":       results["metadatas"][0][i]["type"],
-            "name":       results["metadatas"][0][i]["name"],
-            "start_line": results["metadatas"][0][i]["start_line"],
-            "end_line":   results["metadatas"][0][i]["end_line"],
-            "score":      1 - results["distances"][0][i]  # cosine similarity
+            "code":       decompress_code(r.payload["code"]),
+            "file":       r.payload["file"],
+            "language":   r.payload["language"],
+            "type":       r.payload["type"],
+            "name":       r.payload["name"],
+            "start_line": r.payload["start_line"],
+            "end_line":   r.payload["end_line"],
+            "score":      r.score  # cosine similarity
         })
 
     return chunks 
 
+
+def delete_embeddings(repo_id: str):
+    try:
+        client.delete_collection(f"repo_{repo_id}")
+    except:
+        pass
 
 
