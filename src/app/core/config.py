9875b97deeff445913c25734a0b52da329554d4a
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-04-02 23:37:07 +0530

    fix:dependency issue with workflow

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -3,28 +3,34 @@ from pydantic import ConfigDict
 from dotenv import load_dotenv
 
 load_dotenv()
-
 class Settings(BaseSettings):
 
-    GITHUB_TOKEN: str
-    DATABASE_URL: str
-    # VOYAGE_API_KEY: str
-    GEMINI_API_KEY: str
     ENVIRONMENT: str = "development"
-    EMBEDDING_PROVIDER: str   
-    REDIS_URL: str
-    LLM_PROVIDER: str="groq"
-    GROQ_MODEL: str="llama-3.3-70b-versatile"
-    GROQ_API_KEY: str
-    QDRANT_URL: str
-    QDRANT_API_KEY: str
-    TEST_DATABASE_URL: str
-
-    JWT_SECRET_KEY:     str
-    JWT_ALGORITHM:      str ="HS256"
+    GITHUB_TOKEN: str = "placeholder_token"
+    
+    
+    DATABASE_URL: str = "postgresql://user:password@localhost:5432/db"
+    TEST_DATABASE_URL: str = "postgresql://user:password@localhost:5432/test_db"
+    
+    
+    EMBEDDING_PROVIDER: str = "gemini" 
+    GEMINI_API_KEY: str = "placeholder_key"
+    GROQ_API_KEY: str = "placeholder_key"
+    LLM_PROVIDER: str = "groq"
+    GROQ_MODEL: str = "llama-3.3-70b-versatile"
+    
+    # Vector DB
+    QDRANT_URL: str = "http://localhost:6333"
+    QDRANT_API_KEY: str = "placeholder_key"
+    
+    # Redis
+    REDIS_URL: str = "redis://localhost:6379"
+    
+    # Security
+    JWT_SECRET_KEY: str = "supersecret"
+    JWT_ALGORITHM: str = "HS256"
     JWT_EXPIRE_MINUTES: int = 30
 
-
-    model_config = ConfigDict(env_file=".env")
+    model_config = ConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
