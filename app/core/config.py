f0416ee2cb440a258b22cfbba48b3a12af463b92
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-04-02 22:39:12 +0530

    add: test database initialization

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings(BaseSettings):
+
     GITHUB_TOKEN: str
     DATABASE_URL: str
-    VOYAGE_API_KEY: str
+    # VOYAGE_API_KEY: str
     GEMINI_API_KEY: str
     ENVIRONMENT: str = "development"
     EMBEDDING_PROVIDER: str   
@@ -16,13 +18,13 @@ class Settings(BaseSettings):
     GROQ_API_KEY: str
     QDRANT_URL: str
     QDRANT_API_KEY: str
+    TEST_DATABASE_URL: str
 
     JWT_SECRET_KEY:     str
     JWT_ALGORITHM:      str ="HS256"
     JWT_EXPIRE_MINUTES: int = 30
 
 
-    class Config:
-        env_file = ".env"
+    model_config = ConfigDict(env_file=".env")
 
 settings = Settings()
