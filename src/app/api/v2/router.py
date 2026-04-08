f1f9e80f8b178ec62804191a94ce982b6b55b86a
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-04-08 22:31:01 +0530

    fix: Job not found exception handling

--- src/app/api/v2/router.py
+++ src/app/api/v2/router.py
@@ -1,8 +1,8 @@
-from app.api.v2.endpoints import ingestion
+from app.api.v2.endpoints import ingest
 from fastapi import APIRouter
 
 
 api_router = APIRouter()
 
-api_router.include_router(ingestion.router, prefix="/ingest", tags=["ingestion v2"])
+api_router.include_router(ingest.router, prefix="/ingest", tags=["ingestion v2"])
 
