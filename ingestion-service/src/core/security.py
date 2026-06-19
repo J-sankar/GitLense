bacde5270d4ebf8548a49d45084909b9e07b3ac7
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-06-19 15:26:05 +0530

    fix:refined message

--- ingestion-service/src/core/security.py
+++ ingestion-service/src/core/security.py
@@ -134,7 +134,7 @@ async def get_current_user(
     user = result.scalar_one_or_none()
 
     if not user:
-        logger.error(f"User not found : {user_id}")
+        logger.error(f"User not found: {user_id}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
         )
